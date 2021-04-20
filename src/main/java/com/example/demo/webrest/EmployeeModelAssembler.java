3fd5385e7534fc22360894889fed462716f40670
Urse95 <ursache_alexandru@yahoo.com>
2021-04-20 12:55:01 +0300

    Commit with RestWeb app, EmployeeModelAssembler.java, EmployeeNotFoundAdvice.java + updates in pom and controller

---
+++ src/main/java/com/example/demo/webrest/EmployeeModelAssembler.java
@@ -0,0 +1,20 @@
+package com.example.demo.webrest;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import com.example.demo.domain.Employee;
+import com.example.demo.webrest.controllers.EmployeeController;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmployeeModelAssembler implements RepresentationModelAssembler<Employee, EntityModel<Employee>> {
+
+    @Override
+    public EntityModel<Employee> toModel(Employee employee) {
+        return EntityModel.of(employee, //
+                linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+    }
+}
--- src/main/java/com/example/demo/webrest/controllers/EmployeeController.java
+++ src/main/java/com/example/demo/webrest/controllers/EmployeeController.java
@@ -1,42 +1,56 @@
-package com.example.demo.controllers;
+package com.example.demo.webrest.controllers;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 import com.example.demo.domain.Employee;
 import com.example.demo.exceptions.EmployeeNotFoundException;
 import com.example.demo.repositories.EmployeeRepository;
+import com.example.demo.webrest.EmployeeModelAssembler;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class EmployeeController {
 
     private final EmployeeRepository repository;
 
-    EmployeeController(EmployeeRepository repository) {
+    private final EmployeeModelAssembler assembler;
+
+    EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler) {
         this.repository = repository;
+        this.assembler = assembler;
     }
 
 
     // Aggregate root
     // tag::get-aggregate-root[]
+
     @GetMapping("/employees")
-    List<Employee> all() {
-        return repository.findAll();
+    public CollectionModel<EntityModel<Employee>> all() {
+
+        List<EntityModel<Employee>> employees = repository.findAll().stream() //
+                .map(assembler::toModel) //
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
-    // end::get-aggregate-root[]
 
     @PostMapping("/employees")
     Employee newEmployee(@RequestBody Employee newEmployee) {
         return repository.save(newEmployee);
     }
 
-    // Single item
-
     @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id) {
+    public EntityModel<Employee> one(@PathVariable Long id) {
 
-        return repository.findById(id)
+        Employee employee = repository.findById(id) //
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
+
+        return assembler.toModel(employee);
     }
 
     @PutMapping("/employees/{id}")
